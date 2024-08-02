bc47ec98ada294f5f8e2be6226eae0a17a59a2f4
HaiNhiHE163672 <152832319+nguyenthihainhi@users.noreply.github.com>
2024-08-02 09:15:24 +0700

    Update app.module.ts

--- booking-app/src/app/app.module.ts
+++ booking-app/src/app/app.module.ts
@@ -7,10 +7,7 @@ import { routes } from './app.routes';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
-import { MenubarModule } from 'primeng/menubar';
-import { DropdownModule } from 'primeng/dropdown';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarHeaderComponent } from './nav/nav-bar-header/nav-bar-header.component';
 import { HomeComponent } from './home/home.component';
@@ -30,19 +27,11 @@ import { LoginComponent } from './authentication/login/login.component';
     HttpClientModule, 
     ReactiveFormsModule,
     RouterModule.forRoot(routes, {enableTracing: true}), 
-    ButtonModule,
     HttpClientModule,
-    ToastModule,
-    MenubarModule,
     BrowserAnimationsModule,
-    DropdownModule,
     
   ],
    providers: [],
-   exports: [
-    ToastModule,
-    MenubarModule,
-   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
